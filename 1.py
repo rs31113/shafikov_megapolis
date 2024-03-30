a5dd2b4446829b447c053edbd606f1db3a95298d
Ruslan Shafikov <rs31113@yandex.ru>
2024-03-30 09:41:53 +0300

    first task

---
+++ 1.py
@@ -0,0 +1,21 @@
+source_file = open("songs.txt", encoding="utf-8")
+
+data = [elem.strip().split("?") for elem in source_file.readlines()][1:]
+dictionary = dict()
+
+for elem in data:
+    streams, name, song_name, date = elem
+    if name not in dictionary:
+        dictionary[name] = [[song_name, streams, date]]
+    else:
+        dictionary[name] += [[song_name, streams, date]]
+
+request = input()
+result_file = open("songs_artst.csv", "w", encoding="utf-8")
+result_file.write("track_name,streams,date\n")
+if request in dictionary:
+    for song in dictionary[request]:
+        song = ",".join(song) + "\n"
+        result_file.write(song)
+else:
+    print("No such artist.")
