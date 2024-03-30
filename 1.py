2e96e93a544e335049795ea75d95a1447e95deb4
Ruslan Shafikov <rs31113@yandex.ru>
2024-03-30 09:53:00 +0300

    update first task

--- 1.py
+++ 1.py
@@ -1,8 +1,9 @@
+# Чтение данных из исходного файла
 source_file = open("songs.txt", encoding="utf-8")
-
 data = [elem.strip().split("?") for elem in source_file.readlines()][1:]
 dictionary = dict()
 
+ # Обработка данных, сохранение в словарь с ключом - именем артиста
 for elem in data:
     streams, name, song_name, date = elem
     if name not in dictionary:
@@ -10,6 +11,7 @@ for elem in data:
     else:
         dictionary[name] += [[song_name, streams, date]]
 
+# Запись данных в конечный файл "songs_artst.csv"
 request = input()
 result_file = open("songs_artst.csv", "w", encoding="utf-8")
 result_file.write("track_name,streams,date\n")
